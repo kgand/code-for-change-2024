dae7c5a7964777ab391810e45e4726747491f1a3
Mannvika <mannyjainlakes@gmail.com>
2024-02-10 12:06:23 -0500

    Manav Push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import video from "./video1.mp4"
+import React, {Component} from 'react';
+import ReactScrollWheelHandler from "react-scroll-wheel-handler";
+import ReactPlayer from 'react-player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Feed extends React.Component
+{
+  state = 
+  {
+    currentIndex: 0,
+    videoList : [
+      'https://www.youtube.com/watch?v=7DKv5H5Frt0&ab_channel=TomScott',
+      'https://www.youtube.com/watch?v=F0JDK_71yDg&ab_channel=TomScott',
+      // Add more videos as needed
+    ],
+  };
+  nextIndex = () => {
+    const { videoList, currentIndex } = this.state;
+    if (currentIndex == videoList - 1)
+    {
+      return this.setState({currentIndex: 0});
+    }
+
+    return this.setState({currentIndex: currentIndex + 1,});
+  };
+
+  prevIndex = () => {
+    const { videoList, currentIndex} = this.state;
+    if(currentIndex == 0)
+    {
+      return this.setState({
+        currentIndex : videoList.length - 1,
+      });
+    }
+
+    return this.setState({
+      currentIndex: currentIndex - 1,
+    })
+  }
+  render() {
+    const {videoList, currentIndex} = this.state;
+  
+    return (
+      <div className="App">
+        <ReactScrollWheelHandler
+        upHandler={this.prevIndex}
+        downHandler={this.nextIndex}>
+        <header className="App-header">
+          <ReactPlayer url={videoList[currentIndex]}/>
+        </header>
+        </ReactScrollWheelHandler>
+      </div>
+    );
+  }
+};\
+
+
 
 export default App;
