168ecc1a83ee7440f0710006855e71da9e8ccc23
kgand <34491606+kgand@users.noreply.github.com>
2024-02-10 15:23:46 -0500

    init framework

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+let firebaseConfig = {
+
+};
+
+let firebaseApp = firebase.initializeApp(firebaseConfig);
+export let firebaseAuth = firebaseApp.auth();
+export let firebaseStorage = firebaseApp.storage();
+export let firebaseDB = firebaseApp.firestore();
+export let timeStamp = firebase.firestore.FieldValue.serverTimestamp;
